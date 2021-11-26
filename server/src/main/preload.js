1037ff088becf92c44f9f724ad8dc2507ba7f026
alexthvest <alexthvest@gmail.com>
2021-11-26 18:34:52 +0300

    Added base streaming

--- server/src/main/preload.js
+++ server/src/main/preload.js
@@ -1,8 +1,14 @@
-const { contextBridge, ipcRenderer } = require("electron");
+const { contextBridge, ipcRenderer, desktopCapturer } = require("electron");
 
 contextBridge.exposeInMainWorld("bridge", {
   ipcRenderer: {
     send: (channel, ...args) => ipcRenderer.send(channel, ...args),
-    receive: (channel, listener) => ipcRenderer.on(channel, listener),
+    on: (channel, listener) => ipcRenderer.on(channel, listener),
+  },
+  desktopCapturer: {
+    async getEntireScreenSource() {
+      const sources = await desktopCapturer.getSources({ types: ["screen"] });
+      return sources.find((x) => x.name === "Entire Screen");
+    },
   },
 });
--- server/src/renderer/renderer.ts
+++ server/src/renderer/renderer.ts
@@ -1,8 +1,40 @@
 import "./index.css";
 
-declare const window: { bridge: unknown };
+interface DesktopCapturer {
+  getEntireScreenSource(): Promise<DesktopCapturerSource>;
+}
 
-console.log(window.bridge);
+interface DesktopCapturerSource {
+  readonly id: string;
+}
+
+declare const window: {
+  bridge: {
+    desktopCapturer: DesktopCapturer;
+  };
+};
+
+const { desktopCapturer } = window.bridge;
+
+desktopCapturer.getEntireScreenSource().then(async (source) => {
+  const constraints = {
+    audio: false,
+    video: {
+      mandatory: {
+        chromeMediaSource: "desktop",
+        chromeMediaSourceId: source.id,
+      },
+    },
+  };
+
+  const stream = await navigator.mediaDevices.getUserMedia(
+    constraints as MediaStreamConstraints
+  );
+
+  const video = document.querySelector("video");
+  video.srcObject = stream;
+  video.onloadedmetadata = () => video.play();
+});
 
 console.log(
   '👋 This message is being logged by "renderer.js", included via webpack'
