c5500e0d1d7cadb1c1bb9ba67078a387a69d918d
alexthvest <alexthvest@gmail.com>
2021-11-26 18:04:10 +0300

    Added example of bridging

--- server/src/renderer/renderer.ts
+++ server/src/renderer/renderer.ts
@@ -1,5 +1,9 @@
 import "./index.css";
 
+declare const window: { bridge: unknown };
+
+console.log(window.bridge);
+
 console.log(
   '👋 This message is being logged by "renderer.js", included via webpack'
 );
